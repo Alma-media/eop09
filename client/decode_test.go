cd75d2e84adfd645b2cc5d87b1ec6b7bfd3eda60
Alex Sch <alexander@jexia.com>
2020-12-24 21:07:55 +0200

    codec tests

---
+++ client/decode_test.go
@@ -0,0 +1,46 @@
+package client
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/Alma-media/eop09/proto"
+)
+
+var testJSON = `{"MYTBA":{"code":"55700","name":"Tanjong Baran","city":"Tanjong Baran","province":"Sarawak","country":"Malaysia","timezone":"Asia/Kuala_Lumpur","coordinates":[113.9769444,4.593333299999999],"unlocs":["MYTBA"]},"NOBVK":{"code":"40313","name":"Brevik","city":"Brevik","province":"Telemark","country":"Norway","timezone":"Europe/Oslo","coordinates":[9.7,59.05],"unlocs":["NOBVK"]},"TRBDM":{"code":"48963","name":"Bandirma","city":"Bandirma","province":"Balikesir","country":"Turkey","timezone":"Europe/Istanbul","coordinates":[27.97,40.35],"unlocs":["TRBDM"]}}`
+
+func Test_Decode(t *testing.T) {
+	var (
+		reader = strings.NewReader(testJSON)
+		stream = make(chan *proto.Payload)
+	)
+
+	go func() {
+		if err := Decode(reader, stream); err != nil {
+			t.Errorf("unexpected error: %s", err)
+		}
+
+		close(stream)
+	}()
+
+	var index int
+
+	for decoded := range stream {
+		expected := testPorts[index]
+
+		if decoded.Id != expected.Id {
+			t.Errorf("id does not match: got %q, expected %q", decoded.Id, expected.Id)
+		}
+
+		if !reflect.DeepEqual(decoded.Port, expected.Port) {
+			t.Errorf("port does not match:\n%#v\nexpected:\n%#v\n", decoded.Port, expected.Port)
+		}
+
+		index++
+	}
+
+	if expected := len(testPorts); index != expected {
+		t.Errorf("unexpected index: %d, expected %d", index, expected)
+	}
+}
---
+++ client/encode_test.go
@@ -0,0 +1,74 @@
+package client
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/Alma-media/eop09/proto"
+)
+
+var testPorts = []*proto.Payload{
+	{
+		Id: "MYTBA",
+		Port: &proto.Port{
+			Code:        "55700",
+			Name:        "Tanjong Baran",
+			City:        "Tanjong Baran",
+			Province:    "Sarawak",
+			Country:     "Malaysia",
+			Timezone:    "Asia/Kuala_Lumpur",
+			Coordinates: []float64{113.9769444, 4.593333299999999},
+			Unlocs:      []string{"MYTBA"},
+		},
+	},
+	{
+		Id: "NOBVK",
+		Port: &proto.Port{
+			Code:        "40313",
+			Name:        "Brevik",
+			City:        "Brevik",
+			Province:    "Telemark",
+			Country:     "Norway",
+			Timezone:    "Europe/Oslo",
+			Coordinates: []float64{9.7, 59.05},
+			Unlocs:      []string{"NOBVK"},
+		},
+	},
+	{
+		Id: "TRBDM",
+		Port: &proto.Port{
+			Code:        "48963",
+			Name:        "Bandirma",
+			City:        "Bandirma",
+			Province:    "Balikesir",
+			Country:     "Turkey",
+			Timezone:    "Europe/Istanbul",
+			Coordinates: []float64{27.97, 40.35},
+			Unlocs:      []string{"TRBDM"},
+		},
+	},
+}
+
+func Test_Encode(t *testing.T) {
+	var (
+		buff   bytes.Buffer
+		stream = make(chan *proto.Payload)
+	)
+
+	go func() {
+		for _, payload := range testPorts {
+			stream <- payload
+		}
+
+		close(stream)
+	}()
+
+	if err := Encode(&buff, stream); err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	if actual := strings.Replace(buff.String(), "\n", "", -1); actual != testJSON {
+		t.Errorf("output:\n%s\nwas expected to be:\n%s\n", actual, testJSON)
+	}
+}
